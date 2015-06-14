0786d5dd099dc44aadf450de2de1a4a14201680e
bvaughn <brian.david.vaughn@gmail.com>
2015-06-14 08:56:35 -0700

    Rsync command in Gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@ var gulp = require('gulp');
 var rimraf = require('rimraf');
 var runSequence = require('gulp-run-sequence');
 var sass = require('gulp-sass');
+var shell = require('gulp-shell')
 var watch = require('gulp-watch');
 
 gulp.task('build', [
@@ -16,29 +17,35 @@ gulp.task('clean', function(callback) {
   rimraf('dist', callback);
 });
 
-gulp.task('copyHtml', ['clean'], function () {
+gulp.task('copyHtml', ['clean'], function() {
   return gulp.src(['source/**/*.html'], {
     base: 'source'
   }).pipe(gulp.dest('dist/'));
 });
 
-gulp.task('copyResources', ['clean'], function () {
+gulp.task('copyResources', ['clean'], function() {
   return gulp.src(['images/**/*', 'data/**/*'], {
     base: '.'
   }).pipe(gulp.dest('dist/'));
 });
 
-gulp.task('concatJavascript', ['clean'], function () {
+gulp.task('concatJavascript', ['clean'], function() {
   return gulp.src('source/**/*.js')
     .pipe(concat('built.js'))
     .pipe(gulp.dest('dist/'));
 });
 
-gulp.task('compileCss', ['clean'], function () {
+gulp.task('compileCss', ['clean'], function() {
   return gulp.src('sass/*.scss')
     .pipe(sass())
     .pipe(concat('built.css'))
     .pipe(gulp.dest('dist/'));
 });
 
-// TODO Copy HTML files
+gulp.task('rsync', shell.task(
+  [
+    'gsutil rsync -R . gs://www.briandavidvaughn.com'
+  ],
+  {
+    cwd: 'dist/'
+  }));
