81af16ec6ed2a7425a9a37b281789f800ba738a9
bvaughn <brian.david.vaughn@gmail.com>
2015-06-13 21:54:55 -0700

    Updated Resume

--- dist/built.js
+++ dist/built.js
@@ -136,6 +136,16 @@ angular.module('briandavidvaughn').directive('album', function() {
   };
 });
 
+angular.module('briandavidvaughn').directive('albumCards', function() {
+  return {
+    restrict: 'E',
+    templateUrl: '/components/album-cards/component.html',
+    scope: {
+      band: '='
+    }
+  };
+});
+
 angular.module('briandavidvaughn').directive('band', function($sce, Band) {
   return {
     restrict: 'E',
@@ -155,16 +165,6 @@ angular.module('briandavidvaughn').directive('band', function($sce, Band) {
       } else if ($scope.band) {
         $scope.band.description = $sce.trustAsHtml($scope.band.description);
       }
-    }
-  };
-});
-
-angular.module('briandavidvaughn').directive('albumCards', function() {
-  return {
-    restrict: 'E',
-    templateUrl: '/components/album-cards/component.html',
-    scope: {
-      band: '='
     }
   };
 });
